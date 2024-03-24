220fe9bf1a6608eb741cc29ff270e2320b811bee
AZMY-ABDELKHALEK <azmiabdelkhalek86@gmail.com>
2024-03-24 01:55:30 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const profileRoutes = require("./routes/profiles");
+const qualificationRoutes = require("./routes/qualifications");
+const jobRequestRoutes = require("./routes/jobRequests");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/profiles", profileRoutes);
+app.use("/api/qualifications", qualificationRoutes);
+app.use("/api/job-requests", jobRequestRoutes);
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
